73bb4d28e8aecfd62f324c79c1a92b14a82af4a5
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-27 15:26:19 +0530

    feat: setup react-router-dom package

--- index.tsx
+++ index.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './src/App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
