4ed3837f65684a44f0e5e51abd2fc0b84fcc37a4
Sirajul Arfin <sirajularfin@gmail.com>
2025-04-07 19:09:31 +0530

    feat: revert vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        'src': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src'),
     },
-    define: {
-      'process.env': env,
-    },
-  };
+  },
 });
