6f06cc3dbb889a67c0ad68b93117e6d577e94763
Sirajul Arfin <sirajularfin@gmail.com>
2025-04-07 18:53:12 +0530

    feat: update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        'src': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      'process.env': env,
+    },
+  };
+});
