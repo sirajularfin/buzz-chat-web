ffd5d138306576381baeec8ecb4fd3d167b64b3f
Sirajul Arfin <sirajularfin@gmail.com>
2025-04-08 13:49:11 +0530

    feat: make api call in registration page

--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -14,7 +14,8 @@ import styles from './styles';
 import useRegistration from './useRegistration';
 
 function Registration(): React.ReactElement {
-  const { isDesktop, navigate, validationSchema } = useRegistration();
+  const { isDesktop, navigate, validationSchema, onFormSubmit } =
+    useRegistration();
   const style = styles(isDesktop);
 
   return (
@@ -50,10 +51,10 @@ function Registration(): React.ReactElement {
               password: '',
               confirmPassword: '',
             }}
-            validate={validationSchema}
+            validationSchema={validationSchema}
             onSubmit={(values, { setSubmitting }) => {
+              onFormSubmit(values);
               setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
                 setSubmitting(false);
               }, 400);
             }}
