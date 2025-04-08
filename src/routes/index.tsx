9313ae4c369368647c44c2e440679ac7b935e81e
Sirajul Arfin <sirajularfin@gmail.com>
2025-04-08 13:51:02 +0530

    feat: add protected route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { JSX } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { selectIsLoggedIn } from 'src/redux/slices/auth.slice';
+import { useAppSelector } from 'src/redux/store';
+import Chat from '../pages/chat';
 import Login from '../pages/login';
 import Registration from '../pages/registration';
 import { RoutePath } from './types';
+
+function ProtectedRoute({ element }: { element: JSX.Element }) {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  return isLoggedIn ? element : <Navigate to={RoutePath.ROOT} replace />;
+}
 
 function RoutesConfig(): React.ReactElement {
   return (
     <Routes>
       <Route path={RoutePath.ROOT} element={<Login />} />
       <Route path={RoutePath.REGISTER} element={<Registration />} />
+      <Route
+        path={RoutePath.CHAT}
+        element={<ProtectedRoute element={<Chat />} />}
+      />
+      <Route path="*" element={<Navigate to={RoutePath.ROOT} replace />} />
     </Routes>
   );
 }
