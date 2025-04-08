5a143e5156733e15cb0cf055dbc909a1f258bce7
Sirajul Arfin <sirajularfin@gmail.com>
2025-04-08 13:50:22 +0530

    feat: add isEmpty and formatJson util function

--- src/util/common.util.ts
+++ src/util/common.util.ts
@@ -11,3 +11,20 @@ export const createLinearGradient = (
   const gradientColors = colors.join(', ');
   return `linear-gradient(${angle}deg, ${gradientColors})`;
 };
+
+export const isEmpty = (args: unknown): boolean => {
+  return (
+    args === '' ||
+    args === null ||
+    args === undefined ||
+    (typeof args === 'object' && Object.keys(args).length === 0)
+  );
+};
+
+export function formatJson(args: string) {
+  try {
+    return JSON.stringify(JSON.parse(args), null, 2);
+  } catch {
+    return args;
+  }
+}
